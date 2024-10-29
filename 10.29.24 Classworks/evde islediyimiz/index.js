7df531dccae37d5a00c80382db779c76d58f1371
NihadHesenli <niadfloppa@gmail.com>
2024-10-30 01:41:39 +0400

    nese yazdimda

---
+++ 10.29.24 Classworks/evde islediyimiz/index.js
@@ -0,0 +1,16 @@
+// task-1
+
+// let a = prompt("a ucun deyer ver")
+// let b = prompt("b ucun deyer ver")
+// let c = prompt("c ucun deyer ver")
+
+// if (a > c && b > c) {
+//     alert(`Big sum result ${a} and ${b}`)
+// }else if(a < b && c > a){
+//     alert(`Big sum result ${b} and ${c}`)
+// }else if (a > b && c > b) {
+//     alert(`Big sum result ${a} and ${c}`)
+// }
+
+// task-2
+
