1b9699077dfd6eb8de96e1f22e26e87a606aa2d1
NihadHesenli <mi7rtfggj@code.edu.az>
2024-10-31 19:24:21 +0400

    1233211233214321

---
+++ 10.31.24-Classworks/index.js
@@ -0,0 +1,187 @@
+
+let array = [12,23,43,54,65,67,89,90,100,19]
+let arr = []
+let check;
+
+// task-1
+
+let max = array[0];
+let min = array[0];
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//         arr.push(array[i]) 
+        
+//     }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i]
+//         }
+        
+//     }
+    
+// }
+// console.log(max);
+
+
+// task-2
+
+// for (let i = 0; i < array.length; i++) {
+//     if (i % 2 === 1) {
+//         arr.push(array[i])
+        
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i]
+//         }
+        
+//     }
+// }
+
+// console.log(max);
+
+// task-3
+
+// for (let i = 0; i < array.length; i++) {
+//     if (max < array[i]) {
+//         max=array[i]
+//     }
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     if (min > array[i]) {
+//         min=array[i]
+//     }
+// }
+
+// let minTimesMax = min * max
+// let minPlusMax = min + max
+
+// if (minTimesMax > minPlusMax) {
+//     console.log(minTimesMax);
+    
+// }else{
+//     console.log(minPlusMax);
+    
+// }
+
+// task-4
+
+// let number = 435
+
+// if (number > 0) {
+//         let a = (number - number % 100) / 100;      
+//         let b =  (number % 100-number % 10) / 10;
+//         let c = number % 10;   
+// }           
+
+// console.log(a+b+c);
+
+// task-5
+
+// for (let i = 0; i < array.length; i++) {
+//     if (max < array[i]) {
+//         max=array[i]
+//     }
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     if (min > array[i]) {
+//         min=array[i]
+//     }
+// }
+
+// array.pop()
+
+
+// task-6
+
+// for (let i = 0; i < array.length; i++) {
+//     check =array[i]
+//     for (let j = i + 1; j < array.length; j++) {
+//         if (array[j] === check) {
+//             console.log(check);
+            
+//         }
+        
+//     }
+    
+// }
+
+// task-7
+
+// for (let i = 0; i < array.length; i++) {
+//     if (max < array[i]) {
+//         max=array[i]
+//     }
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     if (min > array[i]) {
+//         min=array[i]
+//     }
+// }
+
+// let sum=0
+// let 
+
+// for (let i = 0; i < array.length; i++) {
+//     sum
+    
+// }
+
+// task-10
+
+// let array1 = [1, 3, 5, 7];
+// let array2 = [2, 3, 6, 7, 8];
+
+
+// for (let i = 0; i < array1.length; i++) {
+//     for (let j = 0; j < array2.length; j++) {
+//         if (array1[i] == array2[j]) {
+//             console.log(array1[i]);    
+//         }   
+//     }
+// }
+
+
+// task-11
+
+let array11 =[-10,12,-23,40,60,73,-6,-20];
+let arrayminus = [];
+let arraymaxim = [];
+
+// for (let i = 0; i < array11.length; i++) {
+//     if (array11[i] < 0) {
+//         arrayminus.length++
+//     }
+// }
+
+// for (let i = 0; i < array11.length; i++) {
+//     if (array11[i] > 0) {
+//         arraymaxim.length++
+//     }
+// }
+
+// console.log(arrayminus.length);
+// console.log(arraymaxim.length);
+
+// task-12
+
+// for (let i = 0; i < array11.length; i++) {
+//     if (array11[i] < 0) {
+//         arrayminus.push(array11[i])
+//     }
+// }
+
+// console.log(arrayminus);
+
+// task-13
+
+let array22 =[11,12,14,56]
+let resarray22 = []
+
+for (let i = array22.length - 1; i >= 0; i--) {
+    
+}
