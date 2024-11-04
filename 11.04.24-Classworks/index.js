3664438b89456bffeef7e0b895c45df86db257fb
NihadHesenli <mi7rtfggj@code.edu.az>
2024-11-04 19:38:27 +0400

    real

---
+++ 11.04.24-Classworks/index.js
@@ -0,0 +1,67 @@
+// let wordFinder = (text,word) =>{
+//     return text.indexOf(word)
+// }
+
+// console.log(wordFinder("lorem ipsum dolor resid", "resid"));
+
+
+// let longTextFinder = (sentence) =>{
+//     const words = sentence.split(" ")
+//     let longestWord = " "
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord.length) {
+//             longestWord = words[i]
+//         }
+        
+//     }
+//     return longestWord
+// }
+// console.log(longTextFinder("Hello my name is Npihad"));
+
+
+// let anagramFinder = (word_1,word_2)=>{
+//     word_1.toLowerCase()
+//     word_2.toLowerCase()
+
+//     if (word_1.length !== word_2.length) {
+//         return false
+//     }
+
+//     if (word_1.split('').sort().join('').toLowerCase() !== word_2.split('').sort().join('').toLowerCase()) {
+//         return false
+//     }else{
+//         return true
+//     }
+
+
+// }
+// console.log(anagramFinder("silent","listen"));
+
+
+// task-1
+
+// let toUpperCase = (sentence) =>{
+//     let uppecase = sentence.toUpperCase()
+//     return uppecase
+// }
+// console.log(toUpperCase("hi nihad"));
+
+// let reversedSentence = (sentence) =>{
+//     const reversed = sentence.split(" ").reverse().join(' ')
+//     return reversed
+// }
+
+// console.log(reversedSentence("Nihad student"));
+
+
+
+
+
+
+// let sorted = (array) =>{
+//     let sortedArray = array.sort((a,b)=>a-b)
+//     return sortedArray
+// }
+// console.log(sorted([12,56,96,47,65,15,20,5]));
+
+
