78c22416839bc0ed5095650dc29795375550d294
NihadHesenli <mi7rtfggj@code.edu.az>
2024-11-06 19:23:46 +0400

    12312321

---
+++ 11.06.24-Classwork/index.js
@@ -0,0 +1,49 @@
+// 1) Write a JavaScript program to create a class called "Person" with properties for name, age and country. Include a method to display the person's details. Create two instances of the 'Person' class and display their details.
+
+// class Person{
+//     constructor(Fname,age,country){
+//         this.name = Fname;
+//         this.age = age;
+//         this.country = country;
+//     }
+// }
+
+// const Person1 = new Person("Nihad" , 19, "Japan")
+// console.log( Person1);
+
+// 2)
+// Bir class yaradirsiz ve o classin icerisinde plus, minus, multiply, divide functionlari olur.
+//          var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2) resultin neticesi bu yazilisa gore 50+6-30*3/2 mentiqi ile (riyazi prinsibi unudun,vurma bolme onceliyine ehtiyac yoxdu) 39 olmalidi.
+
+// class Calculation {
+//     constructor(num) {
+//          this.num = num
+//     }
+
+
+//     plus(value){
+//         this.num += value
+//         return this
+//     }
+    
+//     minus(value){
+//         this.num -= value
+//         return this
+//     }
+
+//     multiply(value){
+//         this.num *= value
+//         return this
+//     }
+
+//     divide(value){
+//         this.num /= value
+//         return this
+//     }
+
+
+// }
+
+// const result = new Calculation(50).plus(6).minus(30).multiply(3).divide(2)
+
+// console.log(result);
