2a64e40c31cf6c10c0738d599230c87cfc1b7601
NihadHesenli <niadfloppa@gmail.com>
2024-12-20 03:51:38 +0400

    editden basqa her sey isleyir

--- December/12.19.2024-Classwork/src/App.jsx
+++ December/12.19.2024-Classwork/src/App.jsx
@@ -1,31 +1,89 @@
-import React, { useState } from "react";
+import  { useEffect, useState } from "react";
 import TodoList from './components/TodoList.jsx'
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import Swal from 'sweetalert2'
 
 function App() {
   const [todos , setTodos] = useState([])
   const [todoInput,settodoInput] = useState("")
   const [filtered , setFiltered] = useState("all")
+  const [searchInput , setSearchInput] = useState("")
+
+  useEffect(()=>{
+    const localData = localStorage.getItem('todos')
+    if (localData) {
+      setTodos(JSON.parse(localData))
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('todos' , JSON.stringify(todos))
+  },[todos])
+  
+  
   const addTodo = ()=>{
     if (todoInput.trim()==='') return;
     setTodos([...todos ,{ id : Date.now() , text : todoInput, isCompleted : false}])
     settodoInput('') 
-    console.log(todos);
-    
+    console.log(todos)
   }
-  const handleInputChange = (e) => {
-    settodoInput(e.target.value);
-  };
+
+  const types = {
+    all : "all",
+    complated : "complated",
+    pending : "pending"
+  }
+
+  const filteredTodos = todos.filter((t)=>{
+    const filteredByTypes = filtered === types.all || (filtered === types.complated && t.isCompleted) || (filtered === types.pending && !t.isCompleted)
+    const filteredBySearchInput = t.text.toLowerCase().includes(searchInput)
+    return filteredByTypes && filteredBySearchInput
+  })
 
   const deleteAll = ()=>{
-    setTodos([])
+    Swal.fire({
+      title: "Are you sure to delete all?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setTodos([])
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
   }
 
   const deleteTodo = (id)=>{
-    const filteredTodos = todos.filter((t) =>t.id !== id)
-    setTodos(filteredTodos)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const filteredTodos = todos.filter((t) =>t.id !== id)
+        setTodos(filteredTodos)
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+    
   }
 
   const complated = (id)=>{
@@ -44,12 +102,17 @@ function App() {
             placeholder="Add a new task"
             className="todo-input"
             value={todoInput}
-            onChange={handleInputChange}
+            onChange={(e)=>{settodoInput(e.target.value)}}
+            onKeyDown={(e)=>{
+              if (e.key === "Enter") {
+                addTodo()
+              }
+            }}
           />
         </Col>
         <Col sm={4} md={3}>
           <Button
-            className="w-30"
+            className="w-30 add"
             variant="primary"
             onClick={addTodo}
           >
@@ -62,6 +125,7 @@ function App() {
         <Col sm="auto">
           <Button
             variant="outline-secondary"
+            onClick={()=>{setFiltered(types.all)}}
           >
             All Todos
           </Button>
@@ -69,6 +133,7 @@ function App() {
         <Col sm="auto">
           <Button
             variant="outline-success"
+            onClick={()=>{setFiltered(types.complated)}}
           >
             Completed Todos
           </Button>
@@ -76,6 +141,7 @@ function App() {
         <Col sm="auto">
           <Button
             variant="outline-warning"
+            onClick={()=>{setFiltered(types.pending)}}
           >
             Pending Todos
           </Button>
@@ -90,7 +156,13 @@ function App() {
           </Button>
         </Col>
       </Row>
-      <TodoList todos={todos} deleteTodo={deleteTodo} complated={complated}/>
+      <Row className="justify-content-end">
+        <Col sm="auto">
+        <input type="text" placeholder="Search your ToDo" className={"search"} onChange={(e)=>{setSearchInput(e.target.value.trim())}}/>
+        </Col>
+      </Row>
+      <TodoList filteredTodos={filteredTodos} deleteTodo={deleteTodo} complated={complated}/>
+      <p className="mt-3">You have <span style={{color:'red'}}>{todos.filter((f)=>{return !f.isCompleted}).length}</span> pending todos</p>
     </Container>
   );
 }
--- December/12.19.2024-Classwork/src/components/TodoList.jsx
+++ December/12.19.2024-Classwork/src/components/TodoList.jsx
@@ -1,24 +1,27 @@
-import React from "react";
 import './TodoList.css'
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import { Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
+import propTypes from 'prop-types';
+import { useState } from 'react';
 
-function TodoList({todos,deleteTodo,complated}) {
+function TodoList({filteredTodos,deleteTodo,complated}) {
+    const [showModal, setShowModal] = useState(false)
     return (
         <>
-        {todos && todos.map((t)=>{
+        {filteredTodos && filteredTodos.map((t)=>{
             return(
                 <div className="list" key={t.id}>
             <div className="info">
  <Form.Check
             type="checkbox"
             onClick={()=>{complated(t.id)}}
+            checked={t.isCompleted}
           />
             <span className={t.isCompleted ? "complated" : ""}>{t.text}</span>
             </div>
             <div className="todo-buttons">
-                <button className="edit-btn">
+                <button className="edit-btn" onClick={()=>{setShowModal(true)}}>
                     <FaEdit />
                 </button>
                 <button className="delete-btn"
@@ -29,8 +32,36 @@ function TodoList({todos,deleteTodo,complated}) {
         </div>
             )
         })}
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Todo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Control
+                        type="text"
+                        placeholder="Edit your todo"
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={()=>{setShowModal(false)}}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary">
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
 
+TodoList.propTypes = {
+    filteredTodos: propTypes.arrayOf(propTypes.shape({
+        id: propTypes.number.isRequired,
+        text: propTypes.string.isRequired,
+        isCompleted: propTypes.bool.isRequired,
+    })).isRequired,
+    deleteTodo: propTypes.func.isRequired,
+    complated: propTypes.func.isRequired,
+};
 export default TodoList;
